859a799e1c73c03c096fd037de6034172f9a0a28
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-08-05 19:48:03 +0530

    basics of JS

---
+++ basics/01.basics.js
@@ -0,0 +1,27 @@
+const accountId = 1200;
+let accountName = "Pradeep Sahu";
+data = "nodata";
+var duplicate;
+console.table([accountId, accountName, data, duplicate]);
+
+let age = 12;
+let name = "Pradeep Sahu";
+let isLoggedIn = false;
+let empty = null;
+
+//number = 2^52
+//bigint
+//string
+//boolean
+//null => standalone value (type) - representation of empty value
+//undefined => value not assigned
+//symbol => to find the uniqueness in react. (component) - unique
+
+//Object
+
+//null is of type of an object
+// undefined is of type undefined
+
+console.table([age, name, isLoggedIn, empty]);
+
+console.log(typeof undefined);
