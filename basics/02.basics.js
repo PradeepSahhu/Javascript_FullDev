f7124c5f40e46b299b28bfe82549324b06465e2c
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-08-06 19:59:45 +0530

    Completed video controllers

--- basics/02.basics.js
+++ basics/02.basics.js
@@ -22,9 +22,58 @@
 // 1 => true, 0=> false
 // ""=> false, "Pradeep"=> true
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+//************************ Operations ******************************************** */
+
+// let value = 3;
+
+// let negValue = -value;
+
+// console.table([value, negValue]);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 4);
+// console.log(2 / 4);
+// console.log(2 % 4);
+
+// let str1 = "Hello";
+
+// let str2 = " Pradeep";
+
+// let str3 = str1 + str2;
+
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + 2 + "2");
+
+// flow of js is left -> right
+
+// console.log(2 + ((4 * 5) % 3));
+
+// console.log(+true); //binary operatory works with numbers.
+
+// console.log(true+); // shows error
+
+// console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+// console.table([num1, num2, num3]);
+
+// let gameCounter = 100;
+// // gameCounter++;
+// // ++gameCounter;
+// console.log(++gameCounter);
+// console.log(gameCounter);
---
+++ basics/03.comparison.js
@@ -0,0 +1,32 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//strict rules that what we can do or what we cant do in js.
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//this type of conversions should be avoided and we must write clean and understandable code
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //null is greater than 0
+
+// == equality operator and >= comparison operator works differently
+// comparisons convert null to a number treating it as 0
+
+// that's why null >=0 is true and null >0 is false
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined > 0);
+
+// console.log(undefined < 0);
+
+//strict check (value + datatype checking)
+
+// console.log("2" == 2);
+// console.log("2" === 2);
