37a30401bcacf1b07fc2bba9adfc05d0de124b9f
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-08-16 21:53:49 +0530

    Completed object

--- basics_2/objects.js
+++ basics_2/objects.js
@@ -9,6 +9,7 @@ const userObj = {
   "Friend Name": ["Soyam", "Surbhi", "Kumar Devashish"],
   age: 21,
   //symbol as key
+  mySymbol: "not so unique",
   [mySymbol]: "unique",
 };
 
@@ -17,3 +18,31 @@ console.log(userObj["name"]);
 console.log(userObj["age"]);
 console.log(userObj["Friend Name"]);
 console.log(userObj[mySymbol]); //whenever using symbol as key inject it inside [] brackets and while displaying value also inject it in the same brackets
+console.log(userObj["mySymbol"]);
+
+// Changing the value of any key
+userObj.name = "Ritik Sahu";
+
+console.log(userObj);
+
+// freezing the object such that it can't be changed later
+// Object.freeze(userObj);
+
+userObj.Group = "B";
+
+console.log(userObj);
+
+// function are treated as type 1 citizen like functions are not discremenated, we can treat them just like variables.
+
+userObj.greeting = function () {
+  console.log("Hello JS user");
+};
+
+userObj.greeting2 = function () {
+  console.log(`Hello js User ${this.name}`);
+};
+// console.log(userObj);
+// console.log(userObj.greeting); // functions reference has been returned
+console.log(userObj.greeting()); // function has been called with undefined
+
+console.log(userObj.greeting2());
