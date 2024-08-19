d9375535253ac345a6cf663cb48d85263ee44f1c
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-08-19 18:13:59 +0530

    forEach and filter

---
+++ Iterators/arrayIterators.js
@@ -0,0 +1,36 @@
+//ForEach
+
+// It doesn't return anyvalue
+const arr = ["Pradeep", "Sumit", "Ritik", "Roshan", "Sandeep"];
+
+// const arrayList = arr.forEach((item, index, fullArr) => {
+//   //   console.log(`${index} --> ${item} --> ${fullArr}`);
+//   return item;
+// });
+
+// //! automatically return when using ()
+// const arrayList = arr.forEach(
+//   (item, index, fullArr) =>
+//     //   console.log(`${index} --> ${item} --> ${fullArr}`);
+//     item
+// );
+
+// console.log(arrayList);
+
+//Filter
+// it returns a value
+
+// const newArray = arr.filter((item) => item.length > 5);
+// console.log(newArray);
+
+// if using {} then we need to return the value. (it means starting a scope)
+// const newArray = arr.filter((item) => {
+//   return item.length > 5;
+// });
+// console.log(newArray);
+
+//Reduce
+
+// reduce will use to add the nubmer (accumulator,item)
+
+//map
