c4163bc54e224285d29e37c91da925df68814933
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-08-20 17:30:24 +0530

    Completed the js basics

--- Iterators/arrayIterators.js
+++ Iterators/arrayIterators.js
@@ -1,7 +1,8 @@
 //ForEach
 
 // It doesn't return anyvalue
-const arr = ["Pradeep", "Sumit", "Ritik", "Roshan", "Sandeep"];
+
+// const arr = ["Pradeep", "Sumit", "Ritik", "Roshan", "Sandeep"];
 
 // const arrayList = arr.forEach((item, index, fullArr) => {
 //   //   console.log(`${index} --> ${item} --> ${fullArr}`);
@@ -23,14 +24,62 @@ const arr = ["Pradeep", "Sumit", "Ritik", "Roshan", "Sandeep"];
 // const newArray = arr.filter((item) => item.length > 5);
 // console.log(newArray);
 
-// if using {} then we need to return the value. (it means starting a scope)
+//! if using {} then we need to return the value. (it means starting a scope) Here we need to explicitely initiate a scope
 // const newArray = arr.filter((item) => {
 //   return item.length > 5;
 // });
 // console.log(newArray);
 
+//? above can be done using forEach by
+
+// const arr = ["Pradeep", "Sumit", "Ritik", "Roshan", "Sandeep"];
+
+// const newNums = [];
+
+// arr.forEach((item) => {
+//   if (item.length > 5) {
+//     newNums.push(item);
+//   }
+// });
+// console.log(newNums);
+
+// const arr = ["Pradeep", "Sumit", "Ritik", "Roshan", "Sandeep"];
+
 //Reduce
 
 // reduce will use to add the nubmer (accumulator,item)
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const summationNumber = arr.reduce(
+  (accumulator, num) => (accumulator += num),
+  0 //initial vlaue of accumulator
+);
+
+console.log(summationNumber);
+
+// const arr = ["Pradeep", "Sumit", "Ritik", "Roshan", "Sandeep"];
+// const num = [1, 2, 3, 4, 5];
+
+//! map also returns a new array after doing some modification
 //map
+
+// const newadded = num.map((item) => item +2);
+// const newadded = num.map((item) => {
+//   return item + 2;
+// });
+//TODO: Both are same
+// console.log(newadded);
+
+//!Chaining
+
+//chaining in arrs looping
+
+// const num = [1, 2, 3, 4, 5];
+
+// const newArray = num
+//   .filter((n) => n % 2)
+//   .map((n) => n + 2)
+//   .map((item) => item * 2);
+
+// console.log(newArray);
