cac285bd32db3a50f05522a0c9e38a7a8d3c01e6
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-11-28 02:48:46 +0530

    xmlHttpRequest

---
+++ Asynccode/console.advance.js
@@ -0,0 +1,13 @@
+//API - it is a taking language between two systems.
+// systems can be frontend , backend, both backend, other's backend and mine frontend.
+
+// most famous api - api.github.com/users/pradeepsahuu
+// Other API -  https://randomuser.me/api/
+//format is JSON
+
+// getting the data from api. - JSONFormatter.
+
+//************************************************************************************************* */
+// [ Not fetch, not axios]
+
+//we will use XMLHTTPRequest - AJAX Programming (Async Javascript and XML  - State)
